8fe9d617ddb5d0e54bfb3956630d3ae887f0c3cd
Mohammad Vaseghi <mvaseghi1@myseneca.ca>
2025-01-07 16:24:14 -0500

    Added lab1b script

---
+++ lab1b.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''description: This script prints a message addressing Isaac.'''
+
+# Assign a string to the object "name"
+name = 'Isaac'
+
+# Print the message using the name object
+print('How old are you ' + name +'?')
+
