4c54e7806c03616278e120d1244d2e8aeb8132e7
Mohammad Vaseghi <mvaseghi1@myseneca.ca>
2025-01-07 17:02:46 -0500

    Added lab1d script(Math in Python)

---
+++ lab1d.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''Description: This script performs a mathematical operation and displays the result.'''
+
+x = 10
+y = 2
+z = 5
+
+
+result = x + y * z
+
+
+print(x,'+',y, '*',z,'=', result)
+
